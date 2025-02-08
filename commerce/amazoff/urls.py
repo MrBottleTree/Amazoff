78a82f176fbac47701a5c96fa6bd8603ee679954
MrBottleTree <vishrut172@gmail.com>
2025-02-08 20:50:26 +0530

    login/logout/register

--- commerce/amazoff/urls.py
+++ commerce/amazoff/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.home, name="home"),
+    path("logout", views._logout, name="logout"),
+    path("login", views._login, name="login"),
+    path("register", views.register_view, name="register")
 ]
--- commerce/amazoff/views.py
+++ commerce/amazoff/views.py
@@ -1,5 +1,63 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import *
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your views here.
-def index(request):
-    return render(request, "amazoff/index.html", {"name":"Vishrut"})
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, "amazoff/home.html", {"name":request.user.username.capitalize()})
+    else:
+        return render(request, "amazoff/login.html", {"message":"To use this website, please login."})
+
+def _logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        message = "You have been logged out successfully!."
+    else:
+        message = "ERROR: You are already logged out."
+    return render(request, "amazoff/login.html", {"message":message})
+
+def _login(request):
+    if(request.method == "POST"):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "amazoff/home.html", {"name":username.capitalize()})
+        else:
+            return render(request, "amazoff/login.html", {"message":"Invalid username or password."})
+    else:    
+        return HttpResponseRedirect(reverse("home"))
+
+def register_view(request, message = "Amazoff, Not so prime!"):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 != password2:
+            message = "Passwords do not match."
+            return HttpResponseRedirect(reverse("register"), args=(message))
+
+        if User.objects.filter(username=username).exists():
+            messages = "Username already taken."
+            return HttpResponseRedirect(reverse("register"), args=(message))
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already registered.")
+            return HttpResponseRedirect(reverse("register"), args=(message))
+
+        # Create user
+        user = User.objects.create_user(username=username, email=email, password=password1)
+        user.save()
+        user = authenticate(username=username, password=password1)
+        if user is not None:
+            login(request, user)
+            return render(request, "amazoff/home.html", {"name":username.capitalize()})
+        else:
+            return render(request, "amazoff/login.html", {"message":"Something went wrong from out side."})
+    return render(request, "amazoff/register.html", {"message":message})
